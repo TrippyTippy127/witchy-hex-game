9b4f1ce8a860aa10ee112fcba80391dec844a7cb
TrippyTippy127 <194023448+TrippyTippy127@users.noreply.github.com>
2026-08-29 20:15:54 -0400

    Made site a single page app

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const navButtons = document.querySelectorAll(".game-nav button");
+const panels = document.querySelectorAll(".game-panel");
 
+navButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const targetPanel = button.dataset.panel;
+
+    panels.forEach((panel) => {
+      panel.classList.remove("active");
+    });
+
+    document.getElementById(targetPanel).classList.add("active");
+  });
+});
